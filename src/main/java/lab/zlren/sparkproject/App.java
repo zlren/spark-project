e6a1ae64102bda6a39c6023fd1bced0668fc7186
zlren <97531314@qq.com>
2017-04-26 11:03:33 +0800

    项目搭建完成，首次提交；完成工具类

---
+++ src/main/java/lab/zlren/sparkproject/App.java
@@ -0,0 +1,7 @@
+package lab.zlren.sparkproject;
+
+/**
+ * Created by zlren on 2017/4/26.
+ */
+public class App {
+}
---
+++ src/main/java/lab/zlren/sparkproject/util/NumberUtils.java
@@ -0,0 +1,24 @@
+package lab.zlren.sparkproject.util;
+
+import java.math.BigDecimal;
+
+/**
+ * 数字格工具类
+ *
+ * @author zlren
+ */
+public class NumberUtils {
+
+    /**
+     * 格式化小数
+     *
+     * @param num   字符串
+     * @param scale 四舍五入的位数
+     * @return 格式化小数
+     */
+    public static double formatDouble(double num, int scale) {
+        BigDecimal bd = new BigDecimal(num);
+        return bd.setScale(scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+
+}
